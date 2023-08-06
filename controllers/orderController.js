d896155780a2e9aa6ae6b664c39cc554ab4bf887
Prashant Chaudhary <donem7510@gmail.com>
2023-08-06 17:47:30 +0545

    ordersavedtoDB

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,62 @@
+// const jwt = require('jsonwebtoken');
+// const config=require('../config/config')
+// const BakeryItem = require('../models/bakeryItem');
+
+// // Place an order for a bakery item (authenticated route)
+// exports.placeOrder = (req, res) => {
+//   const { itemId, quantity } = req.body;
+
+//   // Verify the user token using the authentication middleware
+//   const token = req.header('Authorization');
+
+//   if (!token) {
+//     return res.status(401).json({ error: 'Unauthorized: No token provided.' });
+//   }
+
+//   // Verify the token to check if it's valid and extract the user data
+//   jwt.verify(token, config.JWT_SECRET, (err, decodedToken) => {
+//     if (err) {
+//       return res.status(401).json({ error: 'Unauthorized: Invalid token.' });
+//     }
+
+//     // Token is valid, user is authenticated
+//     // You can access the user data from the decodedToken
+//     const userId = decodedToken.id;
+//     console.log(userId)
+
+//     // Proceed with the order placement logic here
+//     // For example, find the bakery item by ID and create the order
+//     BakeryItem.findById(itemId)
+//       .then((item) => {
+//         if (!item) {
+//           return res.status(404).json({ error: 'Bakery item not found.' });
+//         }
+
+//         // Here, you can save the order in the database or perform any other order-related operations
+//         // For simplicity, we'll just return a response with order details
+//         const order = {
+//           userId, // The authenticated user's ID
+//           itemId: item._id,
+//           itemName: item.name,
+//           quantity,
+//           totalPrice: quantity * item.price, // Calculate totalPrice based on quantity and bakery item price
+//           // Add other order details as needed
+//         };
+
+//         res.json({ message: 'Order placed successfully.', order });
+//       })
+//       .catch((err) => {
+//         res.status(500).json({ error: 'An error occurred while processing the order.' });
+//       });
+//   });
+// };
+
+
+
 const jwt = require('jsonwebtoken');
-const config=require('../config/config')
+const config = require('../config/config');
 const BakeryItem = require('../models/bakeryItem');
+const OrderPlaced = require('../models/orderPlaced'); // Import the OrderPlaced model
 
 // Place an order for a bakery item (authenticated route)
 exports.placeOrder = (req, res) => {
@@ -22,7 +78,7 @@ exports.placeOrder = (req, res) => {
     // Token is valid, user is authenticated
     // You can access the user data from the decodedToken
     const userId = decodedToken.id;
-    console.log(userId)
+    // console.log(userId);
 
     // Proceed with the order placement logic here
     // For example, find the bakery item by ID and create the order
@@ -34,16 +90,21 @@ exports.placeOrder = (req, res) => {
 
         // Here, you can save the order in the database or perform any other order-related operations
         // For simplicity, we'll just return a response with order details
-        const order = {
-          userId, // The authenticated user's ID
-          itemId: item._id,
-          itemName: item.name,
+        const order = new OrderPlaced({
+          user: userId,
+          bakeryItem: item._id,
           quantity,
           totalPrice: quantity * item.price, // Calculate totalPrice based on quantity and bakery item price
-          // Add other order details as needed
-        };
+        });
 
-        res.json({ message: 'Order placed successfully.', order });
+        order
+          .save()
+          .then((savedOrder) => {
+            res.json({ message: 'Order placed successfully.', order: savedOrder });
+          })
+          .catch((err) => {
+            res.status(500).json({ error: 'An error occurred while saving the order.' });
+          });
       })
       .catch((err) => {
         res.status(500).json({ error: 'An error occurred while processing the order.' });
